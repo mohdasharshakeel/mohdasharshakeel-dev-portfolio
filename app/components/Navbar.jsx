2c5f6a07e7822357e3d037f790edae60b88809b9
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-23 18:41:35 +0530

    middle

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+          <nav>
+              <a href="">
+                  <Image src={assets.logo} alt='nameWhite'  className='w-28 cursor-pointer mr-14'/>
+              </a>
+              <ul>
+                  <li><a href="#top">Home</a></li>
+                  <li><a href="#top">ABout me</a></li>
+                  <li><a href="#top">servies</a></li>
+                  <li><a href="#top">My Work</a></li>
+                  <li><a href="#top">Contant me</a></li>
+              </ul>
+              <div>
+                  <a href="#contact">Contact <Image src={assets.arrow_icon} alt='logo' className='w-3 '/></a>
+              </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
