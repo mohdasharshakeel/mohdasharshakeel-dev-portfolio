000ecf9421845ba081f1338efd06b66bc7661ed1
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-25 14:38:16 +0530

    navbar complete

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,18 +5,27 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+      <Image src={assets.header_bg_color} alt='bgc' className='w-full'/>
+    </div>
       <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4  flex items-center justify-between z-50 '>
         <a href="#top">
           <Image src={assets.logo} alt='nameWhite' className='w-28 cursor-pointer mr-14 ' />
         </a>
-        <ul className='hidden md:flex item-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+        <ul className='hidden md:flex item-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50 '>
           <li><a className='font-ovo' href="#top">Home</a></li>
           <li><a className='font-ovo' href="#top">About </a></li>
           <li><a className='font-ovo' href="#top">servies</a></li>
           <li><a className='font-ovo' href="#top">Work</a></li>
           <li><a className='font-ovo' href="#top">Contant</a></li>
         </ul>
-        <div>
+        
+
+        <div className='flex items-center gap-4'>
+          <button>
+            <Image src={assets.moon_icon} alt='' className='w-6 h-6'/>
+          </button>
+       
           <a
             href="#contact"
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo"
@@ -28,7 +37,14 @@ const Navbar = () => {
               className="w-3"
             />
           </a>
+          <button className='block md:hidden ml-3'>
+            <Image src={assets.menu_black} alt='' className='w-6'/>
+          </button>
         </div>
+
+        // Mobile Menu 
+
+        
       </nav>
     </>
   )
