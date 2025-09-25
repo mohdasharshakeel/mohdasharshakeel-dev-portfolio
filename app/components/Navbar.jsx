4ba6345d39ca5f0f59c303d35b363605c0552fd0
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-25 14:18:50 +0530

    updated

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
         <div>
           <a
             href="#contact"
-            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo"
           >
             Contact
             <Image
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        lightHover: "#fcf4ff",
-        darkHover: "#2a004a",
-        darkTheme : '#11001f'
-        
-      },
-      fontfamily: {
-        outfit :['Outfit', 'sans-serif'],
-        ovo : ['Ovo', 'serif' ]
-      }
+export default {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                lightHover: "#fcf4ff",
+                darkHover: "#2a004a",
+                darkTheme: "#11001f",
+            },
+            fontFamily: {
+                ovo: ["var(--font-ovo)", "serif"],
+                outfit: ["var(--font-outfit)", "sans-serif"],
+            }
+
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
