03e362d37abb7e78f82cf339f5aeee6880ce1bdd
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-27 10:06:13 +0530

    updated

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,20 @@
+"use client";
+
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import { useRef } from 'react';
+
 
 const Navbar = () => {
+
+  const sideMenuRef = useRef()
+  const openMenu  = () =>{
+    sideMenuRef.current.style.transform = 'translateX(-16rem)';
+  }
+  const closeMenu = () =>{
+    sideMenuRef.current.style.transform = 'translateX(16rem)';
+  }
+
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -44,7 +56,7 @@ const Navbar = () => {
 
         {/* // Mobile Menu  */} 
 
-        <ul className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
           <div className='position absolute right-6 top-6'>
             <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
           </div>
