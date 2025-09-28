8bff74669eff8b5acac622aad361a42efbcf766c
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-28 13:51:49 +0530

    some css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
+    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4>Who I Am</h4>
       <h2>What I Do</h2>
     </div>
