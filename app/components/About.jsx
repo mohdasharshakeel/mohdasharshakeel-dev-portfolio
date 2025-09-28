7f5298ec7e3a82aabe0761846d868ba19238e2a8
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-28 13:48:53 +0530

    fancy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h4>Introduction</h4>
-      <h2>About me</h2>
+      <h4>Who I Am</h4>
+      <h2>What I Do</h2>
     </div>
   )
 }
