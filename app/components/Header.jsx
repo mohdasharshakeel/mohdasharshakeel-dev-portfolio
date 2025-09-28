ec26e004c162cb6f160a5d86b9fe0d0973d331fc
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-28 12:34:32 +0530

    profile image

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,9 +1,13 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
   return (
     <div>
-      
+      <div> 
+        <Image src={assets.profile-img} alt='' className='rounded-full w-32 '/>
+      </div>
     </div>
   )
 }
