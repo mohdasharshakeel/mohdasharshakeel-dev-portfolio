364545d5de0a65d08252a3159293a52e4fcb6066
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-28 12:50:48 +0530

    contact

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,8 +7,20 @@ const Header = () => {
     <div>
       <div> 
         <Image src={assets.profile-img} alt='' className='rounded-full w-32 '/>
-        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo '>Hi! I'm Mohd Ashar Shakeel  <Image src={assets.hand_icon } alt='' className='  w-6'/></h3>
       </div>
+       <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo '>Hi! I'm Mohd Ashar Shakeel  <Image src={assets.hand_icon } alt='' className='  w-6'/></h3>
+
+<h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
+  Full Stack Web Developer based in India .
+</h1>
+<p className='max-w-2xl mx-auto font-Ovo'>
+  Full Stack Developer skilled in React.js, Node.js, MongoDB, and Next.js, building scalable, high-performance web apps. Experienced in RESTful APIs, AI integrations, and responsive design with Tailwind CSS.
+</p>
+<div>
+  <a href="#contact" >Contact <Image src={assets.right_arrow_white} alt='' className='  w-4 '/></a>
+  <a href="/sample-resume.pdf" download className='px-10 py-3 border-full border-gray-500 flex items-center gap-2 ' >Resume  <Image src={assets.download_icon } alt='' className='  w-4 '/></a>
+</div>
+
     </div>
   )
 }
