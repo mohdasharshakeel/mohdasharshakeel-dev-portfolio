b9685f7d0bb6c509e16d2c960066f468092cd651
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-28 14:31:04 +0530

    updated user image

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,3 +1,5 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const About = () => {
@@ -5,6 +7,14 @@ const About = () => {
     <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg font-Ovo'>Who I Am</h4>
       <h2 className='text-center text-5xl font-Ovo '>What I Do</h2>
+      <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+        <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+            <Image src={assets.user_image} alt='user' className='w-full rounded-3xl'/>
+        </div>
+        <div className='flex-1'>
+
+        </div>
+      </div>
     </div>
   )
 }
