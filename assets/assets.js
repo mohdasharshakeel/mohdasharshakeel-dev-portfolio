a4cd1e0c831ec07c9f5ce7a25e9d667a4e485b94
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-28 15:38:56 +0530

    resume location change

--- assets/assets.js
+++ assets/assets.js
@@ -107,9 +107,9 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Javascript ES6+ Typescript C++ HTML CSS' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Bachelor degree in Computer Application' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Created multiple live projects' }
 ];
 
 export const toolsData = [
