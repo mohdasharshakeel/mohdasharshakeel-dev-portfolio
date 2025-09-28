55a95247c3fa99f3e7eb5b76664a86ee583d64c9
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-28 12:57:30 +0530

    updated

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div>
       <div> 
-        <Image src={assets.profile-img} alt='' className='rounded-full w-32 '/>
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32 '/>
       </div>
        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo '>Hi! I'm Mohd Ashar Shakeel  <Image src={assets.hand_icon } alt='' className='  w-6'/></h3>
 
