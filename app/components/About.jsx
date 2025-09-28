7e259fcc59d461a7407249d40bdc69ffba4a51b6
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-28 14:13:55 +0530

    style

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const About = () => {
   return (
     <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
-      <h4>Who I Am</h4>
-      <h2>What I Do</h2>
+      <h4 className='text-center mb-2 text-lg font-Ovo'>Who I Am</h4>
+      <h2 className='text-center text-5xl font-Ovo '>What I Do</h2>
     </div>
   )
 }
 
 export default About
+ 
