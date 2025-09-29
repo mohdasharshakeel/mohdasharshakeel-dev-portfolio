1f4d285b85da64c357fa70b3c29237acbe6caab0
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-29 16:07:55 +0530

    copy paste

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -2,8 +2,19 @@
  
  const Contact = () => {
    return (
-     <div>
-       
+     <div  id="contact"
+      className="w-full px-4 sm:px-8 md:px-[10%] lg:px-[12%] py-10 scroll-mt-20">
+       <h4 className="text-center mb-2 text-md sm:text-lg font-Ovo">
+        Creative Showcase
+      </h4>
+      <h2 className="text-center text-3xl sm:text-4xl md:text-5xl font-Ovo">
+        Latest Creations
+      </h2>
+      <p className="text-center max-w-xl sm:max-w-2xl mx-auto mt-5 mb-12 text-sm sm:text-base font-Ovo">
+        Step into my web development realm! Explore innovative projects
+        highlighting cutting-edge front-end solutions, interactive UIs, and
+        performance-optimized designs.
+      </p>
      </div>
    )
  }
