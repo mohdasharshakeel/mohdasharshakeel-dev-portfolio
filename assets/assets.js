58df0ebaaae10c02027a701a91d38a35883dc0c1
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-29 09:47:59 +0530

    discription added

--- assets/assets.js
+++ assets/assets.js
@@ -1,164 +1,204 @@
 // ======================= Imports =======================
 
-import user_image from './user-image.png';
-import code_icon from './code-icon.png';
-import code_icon_dark from './code-icon-dark.png';
-import edu_icon from './edu-icon.png';
-import edu_icon_dark from './edu-icon-dark.png';
-import project_icon from './project-icon.png';
-import project_icon_dark from './project-icon-dark.png';
-import vscode from './vscode.png';
-import firebase from './firebase.png';
-import figma from './figma.png';
-import git from './git.png';
-import mongodb from './mongodb.png';
-import right_arrow_white from './right-arrow-white.png';
-import logo from './logo.png';
-import logo_dark from './logo_dark.png';
-import mail_icon from './mail_icon.png';
-import mail_icon_dark from './mail_icon_dark.png';
-import profile_img from './profile_img.png';
-import download_icon from './download-icon.png';
-import hand_icon from './hand-icon.png';
-import header_bg_color from './header-bg-color.png';
-import moon_icon from './moon_icon.png';
-import sun_icon from './sun_icon.png';
-import arrow_icon from './arrow-icon.png';
-import arrow_icon_dark from './arrow-icon-dark.png';
-import menu_black from './menu-black.png';
-import menu_white from './menu-white.png';
-import close_black from './close-black.png';
-import close_white from './close-white.png';
-import web_icon from './web-icon.png';
-import mobile_icon from './mobile-icon.png';
-import ui_icon from './ui-icon.png';
-import graphics_icon from './graphics-icon.png';
-import right_arrow from './right-arrow.png';
-import send_icon from './send-icon.png';
-import right_arrow_bold from './right-arrow-bold.png';
-import right_arrow_bold_dark from './right-arrow-bold-dark.png';
-import javascript_icon from './JavaScript.png';
-import typescript_icon from './TypeScript.png';
-import nodejs_icon from './Node.js.png';
-import expressjs_icon from './Express.png';
-import aws_icon from './AWS.png';
-import docker_icon from './Docker.png';
-import github_icon from './GitHub.png';
-import gitlab_icon from './GitLab.png';
-import react_icon from './ReactIcon.png';
-import nextjs_icon from './Next.js.png';
-import npm_icon from './NPM_icon.png';
-
+import user_image from "./user-image.png";
+import code_icon from "./code-icon.png";
+import code_icon_dark from "./code-icon-dark.png";
+import edu_icon from "./edu-icon.png";
+import edu_icon_dark from "./edu-icon-dark.png";
+import project_icon from "./project-icon.png";
+import project_icon_dark from "./project-icon-dark.png";
+import vscode from "./vscode.png";
+import firebase from "./firebase.png";
+import figma from "./figma.png";
+import git from "./git.png";
+import mongodb from "./mongodb.png";
+import right_arrow_white from "./right-arrow-white.png";
+import logo from "./logo.png";
+import logo_dark from "./logo_dark.png";
+import mail_icon from "./mail_icon.png";
+import mail_icon_dark from "./mail_icon_dark.png";
+import profile_img from "./profile_img.png";
+import download_icon from "./download-icon.png";
+import hand_icon from "./hand-icon.png";
+import header_bg_color from "./header-bg-color.png";
+import moon_icon from "./moon_icon.png";
+import sun_icon from "./sun_icon.png";
+import arrow_icon from "./arrow-icon.png";
+import arrow_icon_dark from "./arrow-icon-dark.png";
+import menu_black from "./menu-black.png";
+import menu_white from "./menu-white.png";
+import close_black from "./close-black.png";
+import close_white from "./close-white.png";
+import web_icon from "./web-icon.png";
+import mobile_icon from "./mobile-icon.png";
+import ui_icon from "./ui-icon.png";
+import graphics_icon from "./graphics-icon.png";
+import right_arrow from "./right-arrow.png";
+import send_icon from "./send-icon.png";
+import right_arrow_bold from "./right-arrow-bold.png";
+import right_arrow_bold_dark from "./right-arrow-bold-dark.png";
+import javascript_icon from "./JavaScript.png";
+import typescript_icon from "./TypeScript.png";
+import nodejs_icon from "./Node.js.png";
+import expressjs_icon from "./Express.png";
+import aws_icon from "./AWS.png";
+import docker_icon from "./Docker.png";
+import github_icon from "./GitHub.png";
+import gitlab_icon from "./GitLab.png";
+import react_icon from "./ReactIcon.png";
+import nextjs_icon from "./Next.js.png";
+import npm_icon from "./NPM_icon.png";
+import ux_icon from "./ux.png" 
+import agile_icon from "./agile.png"
+import database_icon from "./database.png"
+import backend_icon from "./backend.png"
 
 // ======================= Assets Object =======================
 
 export const assets = {
-    user_image,
-    code_icon,
-    code_icon_dark,
-    edu_icon,
-    edu_icon_dark,
-    project_icon,
-    project_icon_dark,
-    vscode,
-    firebase,
-    figma,
-    git,
-    mongodb,
-    right_arrow_white,
-    logo,
-    logo_dark,
-    mail_icon,
-    mail_icon_dark,
-    profile_img,
-    download_icon,
-    hand_icon,
-    header_bg_color,
-    moon_icon,
-    sun_icon,
-    arrow_icon,
-    arrow_icon_dark,
-    menu_black,
-    menu_white,
-    close_black,
-    close_white,
-    web_icon,
-    mobile_icon,
-    ui_icon,
-    graphics_icon,
-    right_arrow,
-    send_icon,
-    right_arrow_bold,
-    right_arrow_bold_dark,
-    javascript_icon,
-    typescript_icon,
-    nodejs_icon,
-    expressjs_icon,
-    aws_icon,
-    docker_icon,
-    github_icon,
-    gitlab_icon,
-    react_icon,
-    nextjs_icon,
-    npm_icon
+  user_image,
+  code_icon,
+  code_icon_dark,
+  edu_icon,
+  edu_icon_dark,
+  project_icon,
+  project_icon_dark,
+  vscode,
+  firebase,
+  figma,
+  git,
+  mongodb,
+  right_arrow_white,
+  logo,
+  logo_dark,
+  mail_icon,
+  mail_icon_dark,
+  profile_img,
+  download_icon,
+  hand_icon,
+  header_bg_color,
+  moon_icon,
+  sun_icon,
+  arrow_icon,
+  arrow_icon_dark,
+  menu_black,
+  menu_white,
+  close_black,
+  close_white,
+  web_icon,
+  mobile_icon,
+  ui_icon,
+  graphics_icon,
+  right_arrow,
+  send_icon,
+  right_arrow_bold,
+  right_arrow_bold_dark,
+  javascript_icon,
+  typescript_icon,
+  nodejs_icon,
+  expressjs_icon,
+  aws_icon,
+  docker_icon,
+  github_icon,
+  gitlab_icon,
+  react_icon,
+  nextjs_icon,
+  npm_icon,
+    ux_icon,
+    agile_icon,
+    database_icon,
+    backend_icon
 };
-
 
 // ======================= Work Data =======================
 
 export const workData = [
-    {
-        title: 'Frontend project',
-        description: 'Web Design',
-        bgImage: '/work-1.png',
-    },
-    {
-        title: 'Geo based app',
-        description: 'Mobile App',
-        bgImage: '/work-2.png',
-    },
-    {
-        title: 'Photography site',
-        description: 'Web Design',
-        bgImage: '/work-3.png',
-    },
-    {
-        title: 'UI/UX designing',
-        description: 'UI/UX Design',
-        bgImage: '/work-4.png',
-    },
+  {
+    title: "Frontend project",
+    description: "Web Design",
+    bgImage: "/work-1.png",
+  },
+  {
+    title: "Geo based app",
+    description: "Mobile App",
+    bgImage: "/work-2.png",
+  },
+  {
+    title: "Photography site",
+    description: "Web Design",
+    bgImage: "/work-3.png",
+  },
+  {
+    title: "UI/UX designing",
+    description: "UI/UX Design",
+    bgImage: "/work-4.png",
+  },
 ];
-
 
 // ======================= Services Data =======================
 
 export const serviceData = [
-    { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
-    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
-    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
-    { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
+  {
+    icon: assets.ux_icon,
+    title: "Frontend ",
+    description: "Where creativity meets code for flawless interfaces...",
+    link: "",
+  },
+  {
+    icon: assets.backend_icon,
+    title: "Backend",
+    description:
+      "The unseen powerhouse behind every modern application...",
+    link: "",
+  },
+  {
+    icon: assets.database_icon,
+    title: "Database Design",
+    description:
+      "Organizing data for reliability, performance, and clarity...",
+    link: "",
+  },
+  {
+    icon: assets.agile_icon,
+    title: "Basic DevOps",
+    description: "Optimizing pipelines for performance, reliability, and speed...",
+    link: "",
+  },
 ];
-
 
 // ======================= Info List =======================
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Javascript ES6+ Typescript C++ HTML CSS' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Bachelor degree in Computer Application' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Created multiple live projects' },
+  {
+    icon: assets.code_icon,
+    iconDark: assets.code_icon_dark,
+    title: "Languages",
+    description: "Javascript ES6+ Typescript C++ HTML CSS",
+  },
+  {
+    icon: assets.edu_icon,
+    iconDark: assets.edu_icon_dark,
+    title: "Education",
+    description: "Bachelor degree in Computer Application",
+  },
+  {
+    icon: assets.project_icon,
+    iconDark: assets.project_icon_dark,
+    title: "Projects",
+    description: "Created multiple live projects",
+  },
 ];
-
 
 // ======================= Tools Data =======================
 
 export const toolsData = [
-    assets.typescript_icon,
-    assets.mongodb,
-    assets.javascript_icon,
-    assets.git,
-    assets.docker_icon,
-    assets.expressjs_icon,
-    assets.react_icon,
-    assets.nodejs_icon,
-    assets.aws_icon,
+  assets.typescript_icon,
+  assets.mongodb,
+  assets.javascript_icon,
+  assets.git,
+  assets.docker_icon,
+  assets.expressjs_icon,
+  assets.react_icon,
+  assets.nodejs_icon,
+  assets.aws_icon,
 ];
