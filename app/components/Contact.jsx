4eca380a017782ca203e93ee8a55e366745106fd
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-29 16:16:08 +0530

    updated

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -5,10 +5,10 @@
      <div  id="contact"
       className=' w-full px-4 sm:px-8 md:px-[10%] lg:px-[12%] py-10 scroll-mt-20 bg-[url("/header-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto ] '>
        <h4 className="text-center mb-2 text-md sm:text-lg font-Ovo">
-        Creative Showcase
+        Connect With Me
       </h4>
       <h2 className="text-center text-3xl sm:text-4xl md:text-5xl font-Ovo">
-        Latest Creations
+        Get In Touch
       </h2>
       <p className="text-center max-w-xl sm:max-w-2xl mx-auto mt-5 mb-12 text-sm sm:text-base font-Ovo">
         Step into my web development realm! Explore innovative projects
