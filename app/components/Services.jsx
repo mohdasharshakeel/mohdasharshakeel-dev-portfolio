dd7120f9f8b030353c0beb61b9f68d3206e73764
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-29 09:08:19 +0530

    commite

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Services = () => {
   return (
     <div id='servies'  className="w-full px-[12%] py-10 scroll-mt-20">
-       <h4 className="text-center mb-2 text-lg font-Ovo">Who I Am</h4>
-      <h2 className="text-center text-5xl font-Ovo ">What I Do</h2>
+       <h4 className="text-center mb-2 text-lg font-Ovo">What I Offer</h4>
+      <h2 className="text-center text-5xl font-Ovo ">Value I Deliver</h2>
       
     </div>
   )
