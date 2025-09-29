f8363aa3d88ad33e34ba924f255eb190a7a46e8a
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-29 10:46:49 +0530

    work

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
+import Work from "./components/Work";
 
 export default function Home() {
   return (
@@ -9,7 +10,9 @@ export default function Home() {
     <Navbar/>
     <Header/>
     <About/>
-    <Services />
+    <Services/>
+    <Work/>
+
     </>
   ); 
 }
