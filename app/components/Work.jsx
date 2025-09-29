e3d2866a98a291d45876c931c25a00b8f388630d
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-29 10:29:53 +0530

    some code

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Work = () => {
   return (
     <div>
-      
+      <h4 className="text-center mb-2 text-md sm:text-lg font-Ovo">What I Offer</h4>
+      <h2 className="text-center text-3xl sm:text-4xl md:text-5xl font-Ovo">Value I Deliver</h2>
+      <p className='text-center max-w-xl sm:max-w-2xl mx-auto mt-5 mb-12 text-sm sm:text-base font-Ovo'></p>
     </div>
   )
 }
