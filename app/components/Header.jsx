ddb8f6aa6462eda1ce77b3372cafb371686446b8
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-02 00:31:10 +0530

    my photo

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,12 +1,16 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
-import motion from  "motion/react"
+import { motion } from "motion/react";
 
 const Header = () => {
   return (
     <div className="w-11/12 max-w-3xl  text-center mx-auto h-screen flex flex-col items-center  justify-center gap-4">
-      <motion.div>
+      <motion.div 
+      initial={{ scale: 0 }}
+      whileInView={{ scale: 1 }}
+      transition={{ duration: 0.8, type: "spring", stiffness: 100  }}
+      >
         <Image src={assets.profile_img} alt="" className="rounded-full w-32 " />
       </motion.div>
       <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo ">
