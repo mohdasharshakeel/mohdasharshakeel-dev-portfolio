f20d61a7bf6cbcf65896df78fb53ae617f140b2d
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-01 11:54:32 +0530

    uploded

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
       </div>
       <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
         <p>© 2025 Ashar Shakeel. All right reserved. </p>
-        <ul>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
             <li><a target="_blank" href="https://github.com/mohdasharshakeel">GitHub</a></li>
             <li><a target="_blank" href="https://www.linkedin.com/in/mohdasharshakeel/">LinkedIn</a></li>
             <li><a target="_blank" href="https://www.instagram.com/orewa.ashar/">Instagram</a></li>
--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -28,7 +28,7 @@ const Services = () => {
           <div
             key={index}
             className="border border-gray-400 rounded-lg px-6 py-8 sm:px-8 sm:py-10 cursor-pointer 
-                       hover:bg-[#fcf4ff] hover:translate-y-1 transition-transform duration-500"
+                       hover:bg-#fcf4ff hover:translate-y-1 transition-transform duration-500"
           >
             <div className="w-12 h-12 sm:w-14 sm:h-14 mx-auto">
               <Image
