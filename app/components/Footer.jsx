d5df3f3e3478e3cabbed1661c5a728ae3119679d
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-01 11:22:39 +0530

    link added

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,15 +1,27 @@
-import { assets } from '@/assets/assets'
-import Image from 'next/image'
-import React from 'react'
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='mt-20'>
-      <div className='text-center '>
-        <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2 '/>
+    <div className="mt-20">
+      <div className="text-center ">
+        <Image src={assets.logo} alt="" className="w-36 mx-auto mb-2 " />
+        <div className="w-max flex items-center gap-2 mx-auto ">
+          <Image src={assets.mail_icon} alt="" className="w-6 " />
+          mohdasharshakeel@gmail.com
+        </div>
+      </div>
+      <div>
+        <p>© 2025 Ashar Shakeel. All right reserved. </p>
+        <ul>
+            <li><a target="_blank" href="https://github.com/mohdasharshakeel">GitHub</a></li>
+            <li><a target="_blank" href="https://www.linkedin.com/in/mohdasharshakeel/">LinkedIn</a></li>
+            <li><a target="_blank" href="https://www.instagram.com/orewa.ashar/">Instagram</a></li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
