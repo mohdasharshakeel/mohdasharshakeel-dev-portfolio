d2543287e4b796cd4e951fd9c249e7254c85726e
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-02 00:35:50 +0530

    remove

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -13,10 +13,10 @@ const Header = () => {
       >
         <Image src={assets.profile_img} alt="" className="rounded-full w-32 " />
       </motion.div>
-      <motion.h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo ">
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo ">
         Hi! I'm Mohd Ashar Shakeel{" "}
         <Image src={assets.hand_icon} alt="" className="  w-6" />
-      </motion.h3>
+      </h3>
 
       <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
         Full Stack Web Developer based in India .
