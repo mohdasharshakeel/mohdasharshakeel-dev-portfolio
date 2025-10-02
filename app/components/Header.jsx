8907f7a8243597468fbbda9dd86d54c61ad9d539
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-02 09:26:50 +0530

    header animation

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,10 +6,10 @@ import { motion } from "motion/react";
 const Header = () => {
   return (
     <div className="w-11/12 max-w-3xl  text-center mx-auto h-screen flex flex-col items-center  justify-center gap-4">
-      <motion.div 
-      initial={{ scale: 0 }}
-      whileInView={{ scale: 1 }}
-      transition={{ duration: 0.8, type: "spring", stiffness: 100  }}
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
       >
         <Image src={assets.profile_img} alt="" className="rounded-full w-32 " />
       </motion.div>
@@ -27,20 +27,26 @@ const Header = () => {
         APIs, AI integrations, and responsive design with Tailwind CSS.
       </p>
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4 ">
-        <a
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1 }}
           href="#contact"
           className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
         >
           Contact{" "}
           <Image src={assets.right_arrow_white} alt="" className="  w-4 " />
-        </a>
-        <a
+        </motion.a>
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1.1 }}
           href="/sample-resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
           Resume <Image src={assets.download_icon} alt="" className="  w-4 " />
-        </a>
+        </motion.a>
       </div>
     </div>
   );
